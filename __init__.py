9f1119e0513bd7a0a67b257f665a9f98d59896a0
babasbelghida <158565982+babasbelghida@users.noreply.github.com>
2024-02-08 16:55:24 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -66,17 +66,17 @@ def Readfiche(post_id):
     # Rendre le template HTML et transmettre les données
     return render_template('read_data.html', data=data)
 
-@app.route('/recherche_fiche_client/<string:name>')
-def recherche_fiche_client(name):
+@app.route('/recheche_fiche_client/<string:name>')
+def Searchfile(name):
     conn = sqlite3.connect('database.db')
     cursor = conn.cursor()
-    cursor.execute('SELECT * FROM clients WHERE name = ?', (name,))
+    param = "%" + name + "%"
+    cursor.execute('SELECT * FROM clients WHERE nom LIKE ?', (param,))
     data = cursor.fetchall()
     conn.close()
-
+    
     # Rendre le template HTML et transmettre les données
-    return render_template('search_data.html', data=data)
-
+    return render_template('read_data.html', data=data)
 if __name__ == '__main__':
     app.run(debug=True)
 
