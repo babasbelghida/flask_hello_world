a33e778431d9b04268f5d93080a846cca8fc2823
babasbelghida <158565982+babasbelghida@users.noreply.github.com>
2024-02-08 15:10:26 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -42,6 +42,10 @@ def meteo():
 def mongraphique():
     return render_template("graphique.html")
 
+@app.route("/histogramme/")
+def monhistogramme():
+    return render_template("histogramme.html")
+
 if __name__ == '__main__':
     app.run(debug=True)
 
