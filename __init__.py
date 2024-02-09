2ec2a27bcf5eaebfca9e2d0dc3a128fe4c93105b
babasbelghida <158565982+babasbelghida@users.noreply.github.com>
2024-02-09 11:18:33 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,3 +1,4 @@
+from flask import Flask, render_template_string, render_template, jsonify
 from flask import render_template
 from flask import json
 from urllib.request import urlopen
@@ -49,3 +50,4 @@ def monfr():
                                                                                                                                        
 if __name__ == "__main__":
   app.run(debug=True)
+
