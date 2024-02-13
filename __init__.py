be918b1b4c39749b4e777416dde825a2c1d45ae6
babasbelghida <158565982+babasbelghida@users.noreply.github.com>
2024-02-13 16:14:33 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -89,92 +89,32 @@ def ReadBDD():
     # Rendre le template HTML et transmettre les données
 
     return render_template('read_data.html', data=data)
-
  
-
-@app.route('/fiche_client/<int:post_id>')
-
-def Readfiche(post_id):
-
-    conn = sqlite3.connect('database.db')
-
-    cursor = conn.cursor()
-
-    cursor.execute('SELECT * FROM clients WHERE id = ?', (post_id,))
-
-    data = cursor.fetchall()
-
-    conn.close()
-
-   
-
-    # Rendre le template HTML et transmettre les données
-
-    return render_template('read_data.html', data=data)
-
- 
-
-@app.route('/fiche_client/<string:post_nom>')
-
-def Searchebyname(post_nom):
-
-    conn = sqlite3.connect('database.db')
-
-    cursor = conn.cursor()
-
-    cursor.execute('SELECT * FROM clients WHERE nom LIKE ?', (post_nom,))
-
-    data = cursor.fetchall()
-
-    conn.close()
-
-   
-
-    # Rendre le template HTML et transmettre les données
-
-    return render_template('read_data.html', data=data)
-
- 
-
 @app.route('/ajouter_client/', methods=['GET', 'POST'])
-
 def ajouter_client():
-
     if request.method == 'POST':
-
         # Récupérer les données du formulaire
-
         nom = request.form['nom']
-
         prenom = request.form['prenom']
-
         adresse = request.form['adresse']
 
- 
-
         # Insérer les données dans la base de données (ici, je suppose que tu as une table 'clients')
-
         conn = sqlite3.connect('database.db')
-
         cursor = conn.cursor()
-
         cursor.execute('INSERT INTO clients (nom, prenom, adresse) VALUES (?, ?, ?)', (nom, prenom, adresse))
-
         conn.commit()
-
         conn.close()
 
- 
-
         # Rediriger vers la page de consultation des clients après l'ajout
-
         return redirect(url_for('ReadBDD'))
 
+    # Si la méthode est GET, simplement rendre le template du formulaire
+    return render_template('ajouter_client.html')
+
+
  
 
-    # Si la méthode est GET, simplement rendre le template du formulaire
 
-    return render_template('ajouter_client.html')
 
  
 
